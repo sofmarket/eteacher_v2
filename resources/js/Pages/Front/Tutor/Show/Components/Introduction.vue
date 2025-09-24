e5fc2151daedf4ddc7722a2bed34a095c002d040
soufiane karroumi <soufiane.karroumi.93@gmail.com>
2025-09-24 18:09:28 +0100

    .

--- resources/js/Pages/Front/Tutor/Show/Components/Introduction.vue
+++ resources/js/Pages/Front/Tutor/Show/Components/Introduction.vue
@@ -9,7 +9,7 @@
                         <div class="flex items-center justify-between">
                             <div class="flex items-center">
                                 <div
-                                    class="w-20 h-20 rounded-lg bg-purple-200 flex items-center justify-center mr-4 flex-shrink-0 relative">
+                                    class="w-18 h-18 rounded-xl bg-purple-200 flex items-center justify-center mr-4 flex-shrink-0 relative">
                                     <svg class="w-12 h-12 text-purple-600" fill="currentColor" viewBox="0 0 20 20">
                                         <path fill-rule="evenodd"
                                             d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
@@ -20,18 +20,18 @@
                                 </div>
                                 <div class="">
                                     <div class="flex items-center mb-1">
-                                        <h1 class="text-3xl text-stone-800">Cynthia Hunter</h1>
+                                        <h1 class="text-2xl font-bold text-stone-800">Cynthia Hunter</h1>
                                         <div class="flex items-center mx-2">
-                                            <svg class="w-6 h-6 text-green-500 mr-2" fill="currentColor"
+                                            <svg class="w-6 h-6 text-green-500" fill="currentColor"
                                                 viewBox="0 0 20 20">
                                                 <path fill-rule="evenodd"
                                                     d="M6.267 3.455a3.066 3.066 0 001.745-.723 3.066 3.066 0 013.976 0 3.066 3.066 0 001.745.723 3.066 3.066 0 012.812 2.812c.051.643.304 1.254.723 1.745a3.066 3.066 0 010 3.976 3.066 3.066 0 00-.723 1.745 3.066 3.066 0 01-2.812 2.812 3.066 3.066 0 00-1.745.723 3.066 3.066 0 01-3.976 0 3.066 3.066 0 00-1.745-.723 3.066 3.066 0 01-2.812-2.812 3.066 3.066 0 00-.723-1.745 3.066 3.066 0 010-3.976 3.066 3.066 0 00.723-1.745 3.066 3.066 0 012.812-2.812zm7.44 5.252a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                                                     clip-rule="evenodd" />
                                             </svg>
-                                            <span class="text-green-600 font-medium">Verified</span>
+                                            <span class="text-green-600 font-medium mx-1">Verified</span>
                                         </div>
                                     </div>
-                                    <p class="text-stone-600">Empowering Students with Customized Learning Support.</p>
+                                    <p class="text-sm text-stone-600">Empowering Students with Customized Learning Support.</p>
                                 </div>
                             </div>
                             <!-- Pricing -->
